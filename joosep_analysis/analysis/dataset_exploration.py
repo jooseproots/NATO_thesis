f7b0ce95c3c293d831a22615f78e07e647eb8db0
Joosep Roots <joosep.roots@gmail.com>
2025-06-13 20:36:29 +0200

    Code to explore dataset and filtered data for regression

---
+++ joosep_analysis/analysis/dataset_exploration.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import scipy.stats
+
+df = pd.read_csv("C:\\Users\\joose\\Git_repos\\NATO_thesis\\joosep_analysis\\clean_data\\final_dataset.csv")
+
+# print(df.info())
+
+# Decribe columns
+columns_to_check = [
+    "Unemployment rate", "Secondary education attainment rate", "GDP per capita",
+    "Defence budget per capita", "Defence budget % GDP", "GDP per capita % change",
+    "Defence budget per capita % change", "Defence budget % GDP % change"
+]
+
+# print(df[columns_to_check].describe())
+
+
+# Z score outliers
+df_clean = df[columns_to_check].dropna()
+
+
+z_scores = df_clean.apply(scipy.stats.zscore)
+
+                                      # 3 is a common threshold
+outliers_z = df_clean[(z_scores.abs() > 3).any(axis=1)]
+# print(outliers_z)
+
+
+# IQR outliers
+def detect_outliers_iqr(df: pd.DataFrame, column: str):
+    Q1 = df[column].quantile(0.25)
+    Q3 = df[column].quantile(0.75)
+    IQR = Q3 - Q1
+    lower_bound = Q1 - 1.5 * IQR
+    upper_bound = Q3 + 1.5 * IQR
+    return df[(df[column] < lower_bound) | (df[column] > upper_bound)]
+
+for col in columns_to_check:
+    outliers = detect_outliers_iqr(df, col)
+    print(outliers[["Country", "Year", col]])
--- joosep_analysis/analysis/regression_analysis.py
+++ joosep_analysis/analysis/regression_analysis.py
@@ -4,6 +4,21 @@ import numpy as np
 
 df = pd.read_csv("C:\\Users\\joose\\Git_repos\\NATO_thesis\\joosep_analysis\\clean_data\\final_dataset.csv")
 
+###############
+# Subset data #
+###############
+
+# Remove data for Russia and China because it might be unreliable
+# Also remove 2024 data as it is mostly filled in/interpolated from 2023 values
+
+df = df[
+    (~df["Country"].isin(["Russia", "China"]))                  # Exclude Russia and China
+    (df["Year"] != 2024)                                        # Exclude year 2024
+    # (df["Country"].isin(["Estonia", "Latvia", "Lithuania"]))    # Test for Baltics
+]
+
+df = df.reset_index(drop=True)
+
 ##############
 # Clean data #
 ##############
@@ -25,7 +40,7 @@ df[numeric_cols] = df[numeric_cols].replace(0, np.nan)
 # Drop rows with missing values
 df_clean = df.dropna(subset=numeric_cols).copy()
 
-log_cols = ['Active Armed Forces per capita', 'Active Armed Forces', 'Population', 'GDP (2015 USD)', 'GDP per capita', 'Defence budget per capita']
+log_cols = ["Active Armed Forces per capita", "GDP per capita", "Defence budget per capita"] # 'Active Armed Forces', 'Population', 'GDP (2015 USD)'
 for col in log_cols:
     df_clean[f"log_{col}"] = np.log(df_clean[col])
 
